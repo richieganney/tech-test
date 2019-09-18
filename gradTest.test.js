1965bc1d1eb468a9be405846c22427538fc75994
richieganney <richieganney@gmail.com>
2019-09-18 14:06:18 +0100

    first simple test passing

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,12 +1,24 @@
-function createMenuData(data) {
- 
- }
+function createMenuData(parent) {
+  let newArr = parent.map(function(parent){
+  subArr = parent.split("/")
+  return {title: subArr[0], data: [subArr[1]]}
+ });
+ return newArr
+}
 
 describe("menu Data Generator", () => {
     it("creates correct data structure ", () => {
       const data = ["parent1/parent1child"]
 
       const expectedResult = [{title: "parent1", data: ["parent1child"]}]
+      const actualResult = createMenuData(data);
+      expect(actualResult).toMatchObject(expectedResult);
+    });
+
+    it("creates correct data structure ", () => {
+      const data = ["parent1/parent1child", "parent1/parent1child2"]
+
+      const expectedResult = [{title: "parent1", data: ["parent1child", "parent1child2"]}]
       const actualResult = createMenuData(data);
       expect(actualResult).toMatchObject(expectedResult);
     });
